430a1142a13e6e184918667d248e4f1644d000d8
Reid Fuhrman <rjfuhrman42@students.tntech.edu>
2020-11-12 22:59:31 -0500

    added a dummy select for the sorting of the main page

--- src/components/GamesList.js
+++ src/components/GamesList.js
@@ -4,6 +4,15 @@ import GameCard from './GameCard';
 function GamesList({games, isLoggedIn}) {
     return(
         <div className="flex flex-1 flex-wrap flex-row m-auto justify-center w-5/6 h-full">
+            <div className="w-full p-4">
+                <div className="float-right mr-48">
+                    <label for="sort">Sort by: </label>
+                    <select name="sort" id="sort">
+                        <option value="popularity">Popularity</option>
+                        <option value="release">Release Date</option>
+                    </select>
+                </div>
+            </div>
             {games.map(game => 
                 <GameCard key={game.id} data={game} isLoggedIn={isLoggedIn}/>
             )}                   
